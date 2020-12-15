df0db5347766e4788dc1576305691d6faec994b0
Lakshmi Girija <41388017+lakshmi-girija-m@users.noreply.github.com>
2020-12-15 15:01:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 from flask import Flask, request, render_template
 import pickle
-from nltk.tokenize import RegexpTokenizer
-from nltk.stem import WordNetLemmatizer,PorterStemmer
 import re
 import contractions
 
 app = Flask(__name__)
 
-stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
-
 def text_preprocess(sent):
-    lemmatizer = WordNetLemmatizer()
-    stemmer = PorterStemmer() 
-    
     # convert all words to lowercase
     sent = sent.lower()
     
@@ -44,17 +37,10 @@ def text_preprocess(sent):
     # remove emails
     sent = re.sub(r"\S*@\S*\s?", '', sent)
     
-    # tokenization
-    tokenizer = RegexpTokenizer(r'\w+')
-    tokens = tokenizer.tokenize(sent.lower())
+    # removing punctuations
+    sent = re.sub(r'[^\w\s]', '', sent) 
     
-    # remove stop words
-    filtered_words = [w for w in tokens if w not in stopwords]
-    
-    stem_words = [stemmer.stem(w) for w in filtered_words]
-    lemma_words = [lemmatizer.lemmatize(w) for w in stem_words]
-    
-    return " ".join(lemma_words)
+    return sent
  
 with open('tfidf.pkl', "rb") as f:
     tfidf = pickle.load(f)
@@ -90,8 +76,6 @@ def predict():
    
    labels = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
    results = [toxic_pred[0][1], severe_toxic_pred[0][1], obscene_pred[0][1], threat_pred[0][1], insult_pred[0][1], identity_hate_pred[0][1]]
-   
-   #final_results = list(zip(labels, results))
    
    return render_template('index.html', labels=labels, results=results)
 
